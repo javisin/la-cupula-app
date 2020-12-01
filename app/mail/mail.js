4f4f680b45203b7d07e3f339088736acd11824ea
Javi Sintes <javisin98@gmail.com>
2020-12-01 18:56:50 +0000

    Add gmail pwd to .env file

--- app/mail/mail.js
+++ app/mail/mail.js
@@ -4,7 +4,7 @@ const transporter = nodemailer.createTransport({
   service: 'gmail',
   auth: {
     user: 'lacupulaebjj@gmail.com',
-    pass: '10Javi98*',
+    pass: process.env.GMAIL_PWD,
   },
 });
 module.exports = transporter;
