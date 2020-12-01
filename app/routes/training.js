3cd80c02995c5b40b375a2f19a47ac078b9a9a5a
Javi Sintes <javisin98@gmail.com>
2020-12-01 18:32:57 +0000

    Add training index endpoint

--- app/routes/training.js
+++ app/routes/training.js
@@ -4,5 +4,6 @@ const router = express.Router();
 const trainingController = require('../controllers/training');
 
 router.post('/trainings', trainingController.create);
+router.get('/trainings/:userId', trainingController.index);
 
 module.exports = router;
