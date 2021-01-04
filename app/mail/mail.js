79c7b2359bc3855c2adb136519ca1878ef48d364
Javi Sintes <javisin98@gmail.com>
2021-01-04 16:46:13 +0000

    Add oauth authentication

--- app/mail/mail.js
+++ app/mail/mail.js
@@ -1,10 +1,24 @@
 const nodemailer = require('nodemailer');
+const { google } = require('googleapis');
+
+const { OAuth2 } = google.auth;
+const oauth2Client = new OAuth2(process.env.CLIENT_ID, process.env.CLIENT_SECRET, 'https://developers.google.com/oauthplayground');
+
+oauth2Client.setCredentials({
+  refresh_token: process.env.REFRESH_TOKEN,
+});
+
+const accessToken = oauth2Client.getAccessToken();
 
 const transporter = nodemailer.createTransport({
   service: 'gmail',
   auth: {
+    type: 'OAuth2',
     user: 'lacupulaebjj@gmail.com',
-    pass: process.env.GMAIL_PWD,
+    clientId: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    refreshToken: process.env.REFRESH_TOKEN,
+    accessToken,
   },
 });
 module.exports = transporter;
