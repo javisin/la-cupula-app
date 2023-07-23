4b54e5a7f2ec82450aa8148e10a527443015804d
Javier Sintes <javisin98@gmail.com>
2023-07-23 21:36:17 +0100

    build: setup prod db connection

--- src/database/models/index.ts
+++ src/database/models/index.ts
@@ -3,32 +3,41 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 const env = process.env.NODE_ENV || 'development';
-if (process.env.DB_DATABASE === undefined) {
-  throw new Error('Missing DB_DATABASE');
-}
-if (process.env.DB_USERNAME === undefined) {
-  throw new Error('Missing DB_USERNAME');
-}
-if (process.env.DB_PASSWORD === undefined) {
-  throw new Error('Missing DB_PASSWORD');
-}
 
 let sequelize: Sequelize;
-// if (env === 'production') {
-//   sequelize = new Sequelize(config.url,
-//     config);
-// } else {
-sequelize = new Sequelize(
-  process.env.DB_DATABASE,
-  process.env.DB_USERNAME,
-  process.env.DB_PASSWORD,
-  {
-    host: 'localhost',
-    dialect: 'postgres',
-    port: 5432,
-  },
-);
-// }
+if (env === 'production') {
+  if (process.env.DB_URL === undefined) {
+    throw new Error('Missing DB_URL');
+  }
+  sequelize = new Sequelize(process.env.DB_URL, {
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
+  });
+} else {
+  if (process.env.DB_DATABASE === undefined) {
+    throw new Error('Missing DB_DATABASE');
+  }
+  if (process.env.DB_USERNAME === undefined) {
+    throw new Error('Missing DB_USERNAME');
+  }
+  if (process.env.DB_PASSWORD === undefined) {
+    throw new Error('Missing DB_PASSWORD');
+  }
+  sequelize = new Sequelize(
+    process.env.DB_DATABASE,
+    process.env.DB_USERNAME,
+    process.env.DB_PASSWORD,
+    {
+      host: 'localhost',
+      dialect: 'postgres',
+      port: 5432,
+    },
+  );
+}
 
 sequelize
   .authenticate()
