f68113c91c4e76f1eacab5e1768d5dc85f1920e6
Javier Sintes <javisin98@gmail.com>
2023-07-23 21:36:17 +0100

    Add verified column in trainings table

--- app/controllers/training.js
+++ app/controllers/training.js
@@ -1,15 +1,52 @@
 const { Training } = require('../database/models');
+const { User } = require('../database/models');
 
 const controller = {
   create: (req, res) => {
     Training.create({
       UserId: req.body.userId,
       date: req.body.date,
+      verified: false,
     })
       .then((training) => res.status(201).json(training))
       .catch((error) => res.status(400).json(error));
   },
   index: (req, res) => {
+    Training.findAll({
+      where: {
+        verified: false,
+      },
+      include: [{ model: User, attributes: ['nickName', 'image'] }],
+      order: [['createdAt', 'ASC']],
+    }).then((trainings) => res.status(200).json(trainings))
+      .catch((error) => res.status(400).json({ error }));
+  },
+
+  update: (req, res) => {
+    Training.findOne({
+      where: {
+        id: req.params.id,
+      },
+    }).then(async (training) => {
+      await training.update({
+        verified: true,
+      });
+      res.status(200).json(training);
+    }).catch(() => res.status(404).json({ message: 'Entrenamiento no encontrado' }));
+  },
+
+  delete: (req, res) => {
+    Training.findOne({
+      where: {
+        id: req.params.id,
+      },
+    }).then(async (training) => {
+      await training.destroy();
+      res.status(200).json(training);
+    }).catch(() => res.status(404).json({ message: 'Entrenamiento no encontrado' }));
+  },
+
+  indexByUser: (req, res) => {
     Training.findAll({
       where: {
         UserId: req.params.userId,
--- app/routes/training.js
+++ app/routes/training.js
@@ -4,6 +4,9 @@ const router = express.Router();
 const trainingController = require('../controllers/training');
 
 router.post('/trainings', trainingController.create);
-router.get('/trainings/:userId', trainingController.index);
+router.put('/trainings/:id', trainingController.update);
+router.delete('/trainings/:id', trainingController.delete);
+router.get('/trainings', trainingController.index);
+router.get('/trainings/:userId', trainingController.indexByUser);
 
 module.exports = router;
