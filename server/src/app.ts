aadd4581189cc9a745493e0e0c8e1b5b83a419ac
Javier Sintes <javisin98@gmail.com>
2023-07-23 21:36:17 +0100

    build: add typescript for web-app

--- server/src/app.ts
+++ server/src/app.ts
@@ -24,9 +24,9 @@ app.use((req, res, next) => {
 });
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../../client/build')));
+  app.use(express.static(path.join(__dirname, '../../web-app/build')));
   app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../../client/build/index.html'));
+    res.sendFile(path.join(__dirname, '../../web-app/build/index.html'));
   });
 }
 
