1b058b22d8aa689f4ac1182c7e7bd5816e66a528
Javier Sintes <javisin98@gmail.com>
2023-07-23 21:36:17 +0100

    Update user attributes returned in index function

--- app/controllers/user.js
+++ app/controllers/user.js
@@ -27,7 +27,7 @@ const controller = {
 
   index: async (req, res) => {
     User.findAll({
-      attributes: ['id', 'firstName', 'lastName', 'belt', 'email', 'nickName', 'image'],
+      attributes: ['id', 'firstName', 'lastName', 'belt', 'stripes', 'nickName', 'image'],
     }).then((users) => res.status(200).json(users));
   },
 
