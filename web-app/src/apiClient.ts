e54cafc0d2b0792d80b9957f34557bccc68d1d32
Javier Sintes <javisin98@gmail.com>
2023-08-10 07:51:23 +0100

    fix: add interceptor to redirect on unauthorized response

--- web-app/src/apiClient.ts
+++ web-app/src/apiClient.ts
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { logout } from './util/auth';
 
 const apiClient = axios.create({
   baseURL: process.env.NODE_ENV !== 'production' ? 'http://localhost:3001/api' : '/api',
@@ -11,5 +12,20 @@ apiClient.interceptors.request.use((config) => {
   }
   return config;
 });
+
+apiClient.interceptors.response.use(
+  function (response) {
+    return response;
+  },
+  function (error) {
+    if (error.response.status === 401) {
+      window.location.replace('/login');
+      logout();
+      return;
+    }
+
+    return Promise.reject(error);
+  },
+);
 
 export default apiClient;
