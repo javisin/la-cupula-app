7f0d86ab7758866d4db70a3ebf431291905101b2
Javier Sintes <javisin98@gmail.com>
2023-09-04 21:27:27 +0200

    feat: add checkBookingsNumber script

---
+++ server/src/app/scripts/checkBookingsNumber.ts
@@ -0,0 +1,61 @@
+import { Op } from 'sequelize';
+import User from '../../database/models/user';
+import { BookingModel } from '../../Context/Bookings/infraestructure/BookingModel';
+import { LessonModel } from '../../Context/Lessons/infraestructure/LessonModel';
+
+process.env.TZ = 'Europe/London';
+
+function getWeekDatesForMonth(start: Date): { startDate: Date; endDate: Date }[] {
+  const month = new Date(start);
+  const result: { startDate: Date; endDate: Date }[] = [];
+  const startDate = new Date(month.getFullYear(), month.getMonth(), 1);
+
+  while (startDate.getMonth() === month.getMonth()) {
+    const endDate = new Date(startDate);
+    endDate.setDate(startDate.getDate() + 6); // End of the week (6 days later)
+    result.push({ startDate: new Date(startDate), endDate });
+
+    startDate.setDate(startDate.getDate() + 7);
+  }
+
+  return result;
+}
+async function main() {
+  const weekDates = getWeekDatesForMonth(new Date('2023-08-01'));
+  // eslint-disable-next-line no-restricted-syntax
+  for (const weekDate of weekDates) {
+    const bookings = await BookingModel.findAll({
+      include: [
+        { model: User, as: 'user' },
+        {
+          model: LessonModel,
+          as: 'lesson',
+          where: {
+            startDate: {
+              [Op.gt]: weekDate.startDate,
+              [Op.lt]: weekDate.endDate,
+            },
+          },
+        },
+      ],
+    });
+    const bookingsByUser: Map<number, number> = new Map();
+    // eslint-disable-next-line no-restricted-syntax
+    for (const booking of bookings) {
+      if (booking.status !== 'approved') {
+        // eslint-disable-next-line no-continue
+        continue;
+      }
+      const userBookings = bookingsByUser.get(booking.userId);
+      if (userBookings) {
+        bookingsByUser.set(booking.userId, userBookings + 1);
+      } else {
+        bookingsByUser.set(booking.userId, 1);
+      }
+    }
+    console.log(bookingsByUser);
+  }
+  process.exit();
+}
+
+main();
