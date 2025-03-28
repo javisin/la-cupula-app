caabee53d719c11973975e52f31d4a882162e589
Javi Sintes <javisin98@gmail.com>
2025-03-28 23:01:32 +0100

    style: change font family to Bebas Neue

--- web-app/src/App.tsx
+++ web-app/src/App.tsx
@@ -10,6 +10,27 @@ const customTheme = createTheme({
       main: '#000000',
     },
   },
+  typography: {
+    fontFamily: '"Bebas Neue", -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen", "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue", sans-serif',
+    h1: {
+      fontFamily: '"Bebas Neue", sans-serif',
+    },
+    h2: {
+      fontFamily: '"Bebas Neue", sans-serif',
+    },
+    h3: {
+      fontFamily: '"Bebas Neue", sans-serif',
+    },
+    h4: {
+      fontFamily: '"Bebas Neue", sans-serif',
+    },
+    h5: {
+      fontFamily: '"Bebas Neue", sans-serif',
+    },
+    h6: {
+      fontFamily: '"Bebas Neue", sans-serif',
+    },
+  },
 });
 
 const queryClient = new QueryClient();
